cd426669ca66fa4249070439c1b09f819597d230
OxBryte <silascyrax@gmail.com>
2025-11-21 00:02:35 +0100

    updated: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import Providers from "./providers";
 import { headers } from "next/headers";
 import { AppKit } from "@reown/appkit/react";
 
