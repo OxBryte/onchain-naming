8ed162e7230162ad43a21df279ebe9c5f80a08b5
OxBryte <silascyrax@gmail.com>
2025-11-21 00:02:30 +0100

    updated: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { headers } from "next/headers";
+import { AppKit } from "@reown/appkit/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
