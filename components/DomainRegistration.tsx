eca7198edbd4dfb51ad3923cb0696ca1e7a2cc16
OxBryte <silascyrax@gmail.com>
2025-11-20 23:55:01 +0100

    updated: app/page.tsx, components/DomainRegistration.tsx

--- components/DomainRegistration.tsx
+++ components/DomainRegistration.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
-import { ConnectButton } from '@reown/appkit/react';
+import { App } from '@reown/appkit/react';
 
 interface DomainRegistrationProps {
   domainName: string;
